36d479446c205607df0c51e78b86b3dc85878507
James Block <james@hewines.com>
2020-03-29 21:37:25 +0100

    first commit - db for packages added

---
+++ db/migrate/20200329202413_create_packages.rb
@@ -0,0 +1,10 @@
+class CreatePackages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :packages do |t|
+      t.string :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
