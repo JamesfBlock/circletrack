501bcaaaa3506e61343bdffbc569735051186f83
James Block <james@hewines.com>
2020-03-29 21:50:00 +0100

    added about us page and footer navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  # root page defined below
   root to: 'pages#home'
+
+  #Any other routes directing to views go below:
+
+  # Route to about page /about
+  get 'about', to: 'pages#about'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
